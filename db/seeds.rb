fa6b22e22c3dae48fdfa459090e406b4326f975d
Jin Qi <jenny_qiqu@hotmail.com>
2022-11-08 22:25:28 -0500

    finished css

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 10.times do
   Article.create(
     title: Faker::Games::Pokemon.move,
-    content: Faker::JapaneseMedia::OnePiece.quote
+    content: Faker::Lorem.paragraph_by_chars(number: rand(256..1000), supplemental: true)
   )
 end
